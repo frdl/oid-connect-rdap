8b9ffd0a57e4c87aaa90872ea1b061eaca030c4c
Till Wehowski <github@webfan.de>
2024-03-11 15:13:50 +0100

    Update RdapProtocolInterface.php

--- src/Webfan/Interfaces/RdapProtocolInterface.php
+++ src/Webfan/Interfaces/RdapProtocolInterface.php
@@ -1,7 +1,9 @@
 <?php
 declare(strict_types=1);
 
-namespace Webfan\RdapClient\Interfaces;
+namespace Webfan\RDAPClient\Interfaces;
+
+use ArrayAccess\RdapClient\Interfaces;
 
 interface RdapProtocolInterface
 {
